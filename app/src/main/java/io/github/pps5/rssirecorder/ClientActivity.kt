551f6357bb9a3284d664c4dc9bccb7153ad525ae
Naoki Nakajima <n2.jima@gmail.com>
2017-05-03 16:58:22 +0900

    Creates activity to show records of Experiments

--- app/src/main/java/io/github/pps5/rssirecorder/ClientActivity.kt
+++ app/src/main/java/io/github/pps5/rssirecorder/ClientActivity.kt
@@ -2,8 +2,14 @@ package io.github.pps5.rssirecorder
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class ClientActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG: String = "ClientActivity"
+        var isScaning: Boolean = false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
